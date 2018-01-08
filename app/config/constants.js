d5ea18784fcd96116e1f788e0250fd4ae58595fa
Daren Capacio <megamanv5@hotmail.com>
2018-01-08 15:11:43 -0800

    Added login function

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyBYeTQk6MKs9BdWkjjzBozPbpZhJyeWVo0";
+export const FIREBASE_AUTH_DOMAIN = "trippi-prototype.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://trippi-prototype.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "trippi-prototype";
+export const FIREBASE_STORAGE_BUCKET = "trippi-prototype.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "402951039348";
+
+export const FACEBOOK_APP_ID = "2088726454681674";
